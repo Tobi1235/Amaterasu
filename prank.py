e94ceca10ce41710377fe74844632466cba39eda
obitobi <54144584+Tobi1235@users.noreply.github.com>
2019-10-27 10:02:57 +0530

    Add files via upload

---
+++ prank.py
@@ -0,0 +1,12 @@
+import os
+try:
+ x=raw_input("""
+LHOST= """)
+ print("""
+Send the direct link to victim:
+""")
+ print("http://"+x+""":8080/fun.html
+""")
+ os.system("apachectl")
+except:
+ print("you have not installed or you written the lhost")
