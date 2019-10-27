9dc90f9bdb4cfe57b02292262e665ca6d606109f
obitobi <54144584+Tobi1235@users.noreply.github.com>
2019-10-27 09:58:18 +0530

    Delete install.py

--- install.py
+++
@@ -1,4 +0,0 @@
-import os
-os.system("mv -f fuck.html /data/data/com.termux/files/usr/share/apache2/default-site/htdocs")
-os.system("mv -f test.html /data/data/com.termux/files/usr/share/apache2/default-site/htdocs")
-
