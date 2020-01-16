9e01fa4b55096191242c61d276dc85387d615f67
obitobi <54144584+Tobi1235@users.noreply.github.com>
2020-01-16 18:38:55 +0530

    Update install.py

--- install.py
+++ install.py
@@ -1,5 +1,4 @@
 import os
 os.system("mv -f fuck.html /data/data/com.termux/files/usr/share/apache2/default-site/htdocs")
-os.system("mv -f test.html /data/data/com.termux/files/usr/share/apache2/default-site/htdocs")
-os.system("mv -f fun.html /data/data/com.termux/files/usr/share/apache2/default-site/htdocs")
+
 
